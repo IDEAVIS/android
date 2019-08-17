1928fd46da614647d50fd19161cadac5212682b9
j4hangir <j4hangir@gmail.com>
2019-08-17 20:13:27 +0200

    fix API url in debug

--- app/src/main/java/com/breadwallet/BreadApp.java
+++ app/src/main/java/com/breadwallet/BreadApp.java
@@ -70,10 +70,14 @@ public class BreadApp extends Application {
         super.onCreate();
         if (Utils.isEmulatorOrDebug(this)) {
 //            BRKeyStore.putFailCount(0, this);
-            HOST = "stage2.breadwallet.com";
+            HOST = "api.piwallet.ideavis.io";
             FirebaseCrash.setCrashCollectionEnabled(false);
 //            FirebaseCrash.report(new RuntimeException("test with new json file"));
         }
+
+        // TODO: 8/17/2019 Perhaps remove this in the future
+        FirebaseCrash.setCrashCollectionEnabled(false);
+
 
         WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
